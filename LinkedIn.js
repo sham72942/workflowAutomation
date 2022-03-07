259128960d3729a6a1425c1e3064bf77c5c42f2d
Shamoel Ahmad <shamoel.ahmad@gmail.com>
2022-03-07 23:06:17 +0530

    Update LinkedIn.js

--- LinkedIn.js
+++ LinkedIn.js
@@ -5,23 +5,25 @@ function search(ariaLabel){
 
 
 //Easy apply to a job
-var easyApply=search('Apply to');
-if(easyApply.length>0){
-    easyApply[0].click();
-    var next=search('Continue to next step');
-    while(next.length>0){
-	next[0].click();
-	next=search('Continue to next step');
-	if(next.length>0) continue;
-	else break;
+async function easyApply(){
+    var easyApply=search('Apply to');
+    if(easyApply.length>0){
+        easyApply[0].click();
+        var next=search('Continue to next step');
+        while(next.length>0){
+        	next[0].click();
+        	next=search('Continue to next step');
+        	if(next.length>0) continue;
+        	else break;
+        }
+    
+        var review=search('Review your application');
+        if(review.length>0) review[0].click();
+        var submit=search('Submit application');
+        if(submit.length>0) submit[0].click();
+        //Sleep for 500ms
+        await new Promise(r => setTimeout(r, 1000));
+        var dismiss=search('Dismiss');
+        if(dismiss.length>0) dismiss[0].click();
     }
-
-    var review=search('Review your application');
-    if(review.length>0) review[0].click();
-    var submit=search('Submit application');
-    if(submit.length>0) submit[0].click();
-    //Sleep for 500ms
-    await new Promise(r => setTimeout(r, 1000));
-    var dismiss=search('Dismiss');
-    if(dismiss.length>0) dismiss[0].click();
 }
